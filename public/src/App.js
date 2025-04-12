af82c94ba71fb972b5681760878c5d93881290f1
thudngm <dngmtthu@gmail.com>
2025-04-12 14:43:29 +0700

    add server/socket.js and update server/index.js

--- public/src/App.js
+++ public/src/App.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SetAvatar from "./components/SetAvatar";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { io } from "socket.io-client";
+
 export default function App() {
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
+
+    // Register this peer with the tracker
+    socket.on("connect", () => {
+      console.log("Connected to tracker");
+      socket.emit("register-peer", { id: socket.id });
+    });
+
+    // Request the list of active peers when needed
+    socket.on("registration-success", () => {
+      console.log("Peer registered successfully");
+      socket.emit("get-peer-list");
+    });
+
+    // Optionally store peerList in state to use in the Chat component
+    socket.on("peer-list", (peerList) => {
+      console.log("Active peers:", peerList);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
